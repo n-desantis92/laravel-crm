0f0f7d4ccfa09309691ef0558719528a464c6cc8
dsn <dsnwebdev@gmail.com>
2021-08-25 11:00:33 +0200

    add: scss show agency
    aggiunto css alla card azienda e aggiunto overflow-y auto alla lista dei dipendenti

--- resources/views/admin/showagency.blade.php
+++ resources/views/admin/showagency.blade.php
@@ -14,7 +14,7 @@
         <div class="form">
             <div class="azienda">
                 <div>
-                    <h3 class="nome-dipendente">{{$agency->name_agency}}</h3>
+                    <h3 class="nome-azienda">{{$agency->name_agency}}</h3>
                     <img class="foto" src="{{asset('storage/' . $agency->logo)}}" alt="foto dipendente">
                 </div>
                 <div class="dati">
@@ -36,29 +36,29 @@
                     @endforeach
                 </select>
             </div>
-            <table class="table table-striped table-bordered table-bost" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Last name</th>
-                        <th>E-mail</th>
-                        <th>Phone</th>
-                        <th>city</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(associato , i) in associati">
-                        <td>@{{associato.name_emp}}</td>
-                        <td>@{{associato.last_name}}</td>
-                        <td>@{{associato.email_emp}}</td>
-                        <td>@{{associato.phone_emp}}</td>
-                        <td>@{{associato.city_emp}}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div class="page">
-
+            <div class="cont-table">
+                <table class="table table-striped table-bordered table-bost " style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Last name</th>
+                            <th>E-mail</th>
+                            <th>Phone</th>
+                            <th>city</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(associato , i) in associati">
+                            <td>@{{associato.name_emp}}</td>
+                            <td>@{{associato.last_name}}</td>
+                            <td>@{{associato.email_emp}}</td>
+                            <td>@{{associato.phone_emp}}</td>
+                            <td>@{{associato.city_emp}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
+
         </div>
     </div>
 
